efea9a873dd60d27edb49f83b3e6c16a99e14a76
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 21:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	let ans= "";
+  for(let i=0;i<s.length;i++){
+      if(s.charAt(i)!=" "){
+          ans+=s.charAt(i);
+      }else{
+          return ans;
+      }
+  }
+  return ans;
 }
 
 // Do not change the code below
